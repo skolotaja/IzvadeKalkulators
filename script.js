04be501b5f726ebf949f03bfb8da801b460a6a80
skolotaja <94806629+skolotaja@users.noreply.github.com>
2022-01-13 23:14:38 +0200

    Gatavs

--- script.js
+++ script.js
@@ -29,8 +29,59 @@ function pusper(m1,m2,m3){
 function laukums(m1,m2,m3){
     const pusper = perimetrs(m1,m2,m3) /2;
     let tlaukums = Math.sqrt(
-      pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3))
-    );
+      pusper * ((pusper - m1) * (pusper - m2) * (pusper - m3)));
     console.log(tlaukums);
     return tlaukums;
   }
+  // Funkcija, kura pārbauda, vai eksistē trijstūris ar nolasītajām malām
+function irTrijsturis(m1, m2, m3) {
+    // pārbauda, vai izpildās trijstūra nevienādība
+    // atgriež true vai false
+    if (m1 < m2 + m3 && m2 < m1 + m3 && m3 < m1 + m2) {
+        return true;
+    } else {
+      return false;
+    }
+  }
+  function rezultats(){
+    const m=nolasa();
+    m1=m.m1;  m2=m.m2; m3=m.m3;
+    console.log(m1, m2, m3);     
+    if (!nolasa()) {
+      t="Nekorekti ievaddati, trijstūra malu vērtībām ir jābūt lielākām par 0";
+      
+    } else {    
+    if (irTrijsturis(m1,m2,m3)==false){
+        t="Trijstūris neeksistē, jo jebkuru 2 malu garumu summai ir jābūt lielākai par trešo malu!";    
+      } else {      
+        t = "Trijstūris ar malu garumiem " + m1 +", " + m2 + " un " + m3 + " eksistē. "
+        if (m1==m2 && m2==m3){
+          t+="Tas ir vienādmalu trijstūris. "
+        } else {
+        if (m1==m2 || m2==m3 || m1==m3){
+          t+="Tas ir vienādsānu trijstūris. "
+        }
+        }
+        const p = perimetrs(m1, m2, m3);
+        const s = Math.round( laukums(m1, m2, m3) * 100) / 100;     
+        t += "Perimetrs ir " + p + " un laukums ir " + s + ".";
+      } 
+      }
+      console.log(t);  
+      return t;
+  }
+  
+  function izvadaTekstu() {
+    // jāizsauc funkcija rezultats()
+    const teksts = rezultats();
+    // elementā ar id "izvade" izvada tekstu
+    console.log(teksts);
+    const sakne = document.getElementById("izvade");
+    const raksti = document.createElement("p");
+    raksti.innerHTML = teksts;
+    sakne.appendChild(raksti);
+  }
+
+ 
+
+  
